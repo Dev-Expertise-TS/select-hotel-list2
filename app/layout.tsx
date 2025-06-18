2660b728a595a0e6fc8dfeed3047fb6293ba5ad4
Dev-Expertise-TS <dev.expertise.ts@gmail.com>
2025-06-18 10:54:16 +0900

    메타 데이터 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: '럭셔리 셀렉트 AI 컨시어지',
+  description: '럭셔리 셀렉트 호텔의 혜택을 확인하시고 호텔의 상세 소개 및 요금을 확인하세요. ',
+  generator: '타이드스퀘어',
 }
 
 export default function RootLayout({
