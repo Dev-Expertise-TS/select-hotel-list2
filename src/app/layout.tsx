36b40dc6f00dd7664a24bc5a0357f8696a7c4b64
Dev-Expertise-TS <dev.expertise.ts@gmail.com>
2025-07-07 15:29:01 +0900

    메타데이터의 제목과 설명을 '투어비스 럭셔리 셀렉트 AI 컨시어지'로 변경하여 브랜드 정체성을 강화하고, 제공하는 서비스에 대한 정보를 명확히 하였습니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '럭셔리 셀렉트 AI 컨시어지',
-  description: '럭셔리 셀렉트 호텔의 혜택을 확인하시고 호텔의 상세 소개 및 요금을 확인하세요. ',
+  title: '투어비스 럭셔리 셀렉트 AI 컨시어지',
+  description: '투어비스 럭셔리 셀렉트 AI 컨시어지가 엄선한 전 세계 고급 호텔을 소개합니다. 프리미엄 호텔들에 대한 투어비스 럭셔리 셀렉트만의 특별한 혜택도 확인하실 수 있습니다.',
   generator: '타이드스퀘어',
 }
 
